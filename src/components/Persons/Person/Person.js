86061a11b6ea534cecf7c8020d0659951dbd5455
IshantL <ishantlambhate@gmail.com>
2019-09-03 12:54:34 +0530

    Ref 2nd type to use ref's

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,8 +4,13 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props){
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
   componentDidMount(){
-    this.inputElement.focus(); 
+    //this.inputElement.focus(); 
+    this.inputElementRef.current.focus();
   }
   render() {
     console.log('[Person.js] rendering...');
@@ -17,7 +22,8 @@ class Person extends Component {
       </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}
-        ref = {(inputEl)=>{this.inputElement = inputEl}}
+        //ref = {(inputEl)=>{this.inputElement = inputEl}}
+        ref ={this.inputElementRef}
         />
       </Aux>
     );
